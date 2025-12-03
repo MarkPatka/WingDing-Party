5a1fafeae10a0bbd59c246e475da80e0ef7456eb
Magad Mammaev <MMammaev@it-one.ru>
2025-12-03 10:07:59 +0300

    Add swagger with basic controller

---
+++ WingDing-Party/Model/ResponseServer.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace WingDing_Party.Model
+{
+    public class ResponseServer
+    {
+        public ResponseServer()
+        {
+            this.IsSuccess = true;
+            this.ErrorMessage = new();
+        }
+
+        public bool IsSuccess { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public List<string> ErrorMessage { get; set; }
+        public object? Result { get; set; }
+    }
+}
---
+++ WingDing-Party/Student.cs
@@ -0,0 +1,7 @@
+namespace WingDing_Party
+{
+    public class Student
+    {
+        public string Name { get; set; }
+    }
+}
