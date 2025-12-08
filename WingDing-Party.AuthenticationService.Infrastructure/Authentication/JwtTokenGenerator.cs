80d779e94719905f00c1cfcc18b22dff5359b468
Mark Patka <mark.kaz.1998@gmail.com>
2025-12-08 18:54:20 +0300

    Implements All Application Interfaces Logic

---
+++ WingDing-Party.AuthenticationService.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WingDing_Party.AuthenticationService.Application.Common.Interfaces.Authentication;
+using WingDing_Party.AuthenticationService.Application.Common.Interfaces.Services;
+
+namespace WingDing_Party.AuthenticationService.Infrastructure.Authentication;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly IDateTimeProvider _timeProvider;
+    private readonly JwtSettings _jwtSettings;
+
+    public JwtTokenGenerator(
+        IDateTimeProvider timeProvider, 
+        IOptions<JwtSettings> jwtSettings)
+    {
+        _timeProvider = timeProvider;
+        _jwtSettings = jwtSettings.Value;
+    }
+
+    public string GenerateToken(Guid userId, string firstName, string lastName)
+    {
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_jwtSettings.Secret)),
+            SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, firstName + " " + lastName),
+            new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var securityToken = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            expires: _timeProvider.UtcNow
+                .AddMinutes(_jwtSettings.ExpiryMinutes),
+            claims: claims,
+            signingCredentials: signingCredentials);
+
+       return new JwtSecurityTokenHandler().WriteToken(securityToken);  
+    }
+}
---
+++ WingDing-Party.AuthenticationService.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using WingDing_Party.AuthenticationService.Application.Common.Interfaces.Services;
+
+namespace WingDing_Party.AuthenticationService.Infrastructure.Services;
+
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime Now => TimeProvider.System.GetLocalNow().DateTime;
+    public DateTime UtcNow => TimeProvider.System.GetUtcNow().DateTime;
+}
