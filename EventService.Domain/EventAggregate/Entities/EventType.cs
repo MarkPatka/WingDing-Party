621873cb84ab9aa955c86fbe96f0dd0910e19d7c
Magad Mammaev <MMammaev@it-one.ru>
2026-02-22 21:41:14 +0300

    Updated Value Objects, Child Entities (2.1, 2.2, 2.3, 2.4)

--- EventService.Domain/EventAggregate/Entities/EventType.cs
+++ EventService.Domain/EventAggregate/Entities/EventType.cs
@@ -6,10 +6,36 @@ namespace EventService.Domain.EventAggregate.Entities;
 public sealed class EventType : Entity<EventTypeId>
 {
     public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public string Icon { get; set; } = string.Empty;
+    public string? Description { get; set; } = null;
+    public string? Icon { get; set; } = null;
 
-    private EventType() { }
-    // TO FDO Create method
-    public EventType(EventTypeId id) : base(id) { }
+    private EventType(Guid id,
+        string name,
+        string? description,
+        string? icon)
+        : base(EventTypeId.Create(id))
+    {
+        Name = name;
+        Description = description;
+        Icon = icon;
+    }
+
+    private EventType(
+        string name,
+        string? description,
+        string? icon)
+        : base(EventTypeId.CreateUnique())
+    {
+        Name = name;
+        Description = description;
+        Icon = icon;
+    }
+
+    public static EventType CreateNew(
+        Guid id, string name, string? description, string? icon)
+            => new(id, name, description, icon);
+
+    public static EventType Create(
+        string name, string? description, string? icon)
+            => new(name, description, icon);
 }
--- EventService.Domain/EventAggregate/Entities/Participant.cs
+++ EventService.Domain/EventAggregate/Entities/Participant.cs
@@ -6,14 +6,52 @@ namespace EventService.Domain.EventAggregate.Entities;
 
 public sealed class Participant : Entity<ParticipantId>
 {
-    public EventId EventId { get; set; } = null!;
-    public string UserId { get; set; } = string.Empty;
-    public string FullName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public DateTime RegisteredAt { get; set; }
-    public ParticipantStatus Status { get; set; } = ParticipantStatus.Registered;
+    public EventId EventId  { get; private set; } = null!;
+    public UserId UserId    { get; private set; } = null!;
+
+    public string UserName          { get; private set; } = string.Empty;
+    public DateTime RegisteredAt    { get; private set; }
+    public ParticipantStatus Status { get; private set; } = null!;
 
     private Participant() { }
 
-    public Participant(ParticipantId id) : base(id) { }
+    public Participant(
+        ParticipantId id,
+        EventId eventId,
+        UserId userId,
+        string userName,
+        DateTime registeredAt
+        ) 
+        : base(id) 
+    {
+        EventId = eventId;
+        UserId = userId;
+        UserName = userName;
+        RegisteredAt = registeredAt;
+        Status = ParticipantStatus.Registered;
+    }
+
+    public static Participant Create(
+        EventId eventId, 
+        UserId userId, 
+        string userName, 
+        DateTime registeredAt)
+    {
+        return new Participant(
+            ParticipantId.CreateUnique(),
+            eventId,
+            userId,
+            userName,
+            registeredAt);
+    }
+
+    public void MarkAsAttented()
+    {
+        Status = ParticipantStatus.Attended;
+    }
+
+    public void Cancel()
+    {
+        Status = ParticipantStatus.Cancelled;
+    }
 }
