cc7cc5460dac2123d641d90d83f1ee3ab2337fdc
Mark Patka <mark.kaz.1998@gmail.com>
2026-04-23 21:55:56 +0300

    Scaffolds Basic Domain Layer Structure and Models

---
+++ src/AuthService/AuthService.Domain/Entities/Role.cs
@@ -0,0 +1,20 @@
+﻿using AuthService.Domain.Common.Abstractions;
+using AuthService.Domain.Enumerations;
+using AuthService.Domain.ValueObjects.Ids;
+
+namespace AuthService.Domain.Entities;
+
+public sealed class Role : Entity<RoleId>
+{
+    private readonly List<User> _users = [];
+    private readonly List<Permission> _permissions = [];
+
+    public IReadOnlyCollection<User> Users => [.. _users];
+    public IReadOnlyCollection<Permission> Permissions => [.. _permissions];
+    public RoleType RoleType { get; private set; } = null!;
+
+    private Role() { }
+    private Role(RoleType roleType) { RoleType = roleType; }
+
+    public static Role Create(RoleType role) => new(role);
+}
