54ba682d25d004f4140822573acbdc83d66b9886
Mark Patka <mark.kaz.1998@gmail.com>
2026-05-03 17:45:24 +0300

    Adds Application Interfaces Implementations

--- src/AuthService/AuthService.Application/Services/IAuthenticationService.cs
+++ src/AuthService/AuthService.Application/Services/IAuthenticationService.cs
@@ -1,5 +1,8 @@
-﻿namespace AuthService.Application.Services;
+﻿using AuthService.Domain.Entities;
+
+namespace AuthService.Application.Services;
 
 public interface IAuthenticationService
 {
+    Task<string> RegisterAsync(User user, string password, CancellationToken ct = default);
 }
--- src/AuthService/AuthService.Application/Services/IJwtService.cs
+++ src/AuthService/AuthService.Application/Services/IJwtService.cs
@@ -2,4 +2,5 @@
 
 public interface IJwtService
 {
+    Task<string?> GetAccessTokenAsync(string email, string password, CancellationToken ct = default);
 }
