6e9675a65c01ced2b18b5321e2af741869825922
Mark Patka <mark.kaz.1998@gmail.com>
2026-06-17 15:59:34 +0300

    fix: removes database init script

--- .containers/postgres-init/init-databases.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE userdb;
-    CREATE DATABASE authdb;
-    CREATE DATABASE eventdb;
-    CREATE DATABASE clubdb;
-EOSQL
