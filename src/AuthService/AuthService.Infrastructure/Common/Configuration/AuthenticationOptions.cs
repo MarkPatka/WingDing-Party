9067e9b91b1150280ddabfc5e0aa75277439eab8
Mark Patka <mark.kaz.1998@gmail.com>
2026-06-17 13:41:54 +0300

    adds RequireHttpsMetadata line to config mapping class

--- src/AuthService/AuthService.Infrastructure/Common/Configuration/AuthenticationOptions.cs
+++ src/AuthService/AuthService.Infrastructure/Common/Configuration/AuthenticationOptions.cs
@@ -9,5 +9,6 @@ public sealed class AuthenticationOptions
     public string Audience { get; set; } = string.Empty;
     public string Issuer { get; set; } = string.Empty;
     public string MetadataUrl { get; set; } = string.Empty;
+    public string AuthServiceGrpcUrl { get; set; } = string.Empty;
     public bool RequireHttpsMetadata { get; init; }
 }
